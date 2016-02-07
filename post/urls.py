b15109ccd9a3502dadaaf75cb516ffa8b8cfa020
Jorge Galvis <jorlugaqui@gmail.com>
2016-02-06 22:53:56 -0500

    Make urlpatterns a list in post/urls.py

--- post/urls.py
+++ post/urls.py
@@ -4,11 +4,10 @@ from .views import PostListView, PostCreateView, PostDetailView, \
     PostUpdateView, PostDeleteView
 
 
-urlpatterns = patterns(
-    'post',
+urlpatterns = [
     url(r'^$', PostListView.as_view(), name='list'),
     url(r'^create/$', PostCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', PostUpdateView.as_view(), name='update'),
     url(r'^(?P<pk>\d+)/delete/$', PostDeleteView.as_view(), name='delete'),
-)
+]
